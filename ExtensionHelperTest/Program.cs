3f54ecd84b7f53841478bcb49d2be73b1d35ddc2
esercengiz44 <eser.cengiz@hotmail.com>
2020-12-24 00:42:42 +0300

    First Commit

---
+++ ExtensionHelperTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExtensionHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionHelperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x = 3843748;
+            Console.WriteLine(x.ToFileSize());
+
+            DateTime birthdate = new DateTime(1960, 6, 13);
+            Console.WriteLine(birthdate.CalculateAge());
+        }
+    }
+}
