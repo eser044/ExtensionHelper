1bbd2106cd2ee336f7d978f5d9f9e9ac7ec07030
esercengiz44 <eser.cengiz@hotmail.com>
2020-12-27 21:20:35 +0300

    unused namsapces removed

--- ExtensionHelper/ExtensionMethods.cs
+++ ExtensionHelper/ExtensionMethods.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExtensionHelper
 {
