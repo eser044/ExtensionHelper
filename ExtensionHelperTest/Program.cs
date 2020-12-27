f3faecb2afdb91c878290ec0d882352b83337a13
esercengiz44 <eser.cengiz@hotmail.com>
2020-12-27 19:58:22 +0300

    upgreade .net 5.0

--- ExtensionHelperTest/Program.cs
+++ ExtensionHelperTest/Program.cs
@@ -1,9 +1,5 @@
-﻿using ExtensionHelper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using ExtensionHelper;
 
 namespace ExtensionHelperTest
 {
