244cd7d3cd38ec0e49612b746415802fcfdbffde
esercengiz44 <eser.cengiz@hotmail.com>
2021-05-24 13:27:29 +0300

    Enum : MetaDataHelper added to proejct

---
+++ ExtensionHelper/MetadataAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionHelper
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+    public class MetadataAttribute : Attribute
+    {
+        public MetadataAttribute(string description, string metaData = "")
+        {
+            Description = description;
+            MetaData = metaData;
+        }
+
+        public string Description { get; set; }
+        public string MetaData { get; set; }
+    }
+}
