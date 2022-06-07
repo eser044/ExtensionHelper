324a88cce23ad2f65460d2f0d71c7917795b6d81
esercengiz44 <eser.cengiz@hotmail.com>
2022-06-08 00:34:58 +0300

    Singleton fixing and add new singleton

---
+++ DotNetFrameWork/HelperUtilities/Helper/Singleton2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperUtilities.Helper
+{
+    public class Singleton2<T> where T : class, new()
+    {
+        private Singleton2() { }
+
+        private static readonly Lazy<T> instance = new Lazy<T>(() => new T());
+
+        public static T Instance { get { return instance.Value; } }
+    }
+}
